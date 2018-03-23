a98197446dc7690929d50e027de49137ee3f44ed
sasitech17 <sasitech17@gmail.com>
2018-03-23 18:10:22 +0530

    Adding some practicals

---
+++ Basic Practices/NamePrint.java
@@ -0,0 +1,7 @@
+public class NamePrint{
+	String name = "Siraj Msm";
+	public static void main(String args[]){
+		NamePrint np = new NamePrint();
+		System.out.println("My name is "+np.name);
+	}
+}
