155ea273dc92a5b8161f72a5cbdb7ad1cc5f705a
sasitech17 <sasitech17@gmail.com>
2018-03-24 00:44:01 +0530

    operator overloading

---
+++ OOP Concept/Polymorphism/Operator_overloading.java
@@ -0,0 +1,10 @@
+class Operator_overloading{
+	public static void main (String args[]){
+		// plus operator only support operator overloading in java
+		// generally operator overloading is not support in java
+		
+		System.out.println(10+60);
+		System.out.println(10+"Siraj");
+	}
+
+}
